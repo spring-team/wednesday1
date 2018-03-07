8a58e49d335b550df2eae369c1161a7dc3735133
Rod Johnson <johnsonroda@gmail.com>
2018-03-07 10:22:38 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/ubs/Wednesday1Application.java
@@ -0,0 +1,12 @@
+package com.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wednesday1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wednesday1Application.class, args);
+	}
+}
