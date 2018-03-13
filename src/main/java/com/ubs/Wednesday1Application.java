84525bef2616e2da12d94fe26abd02caceab4357
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 19:56:01 +1100

    Everyone needs encouragement to write Java

--- src/main/java/com/ubs/Wednesday1Application.java
+++ src/main/java/com/ubs/Wednesday1Application.java
@@ -1,3 +1,5 @@
+// I come from the loving space of my heart, and I know that love opens all doors.
+
 // It’s only a thought, and a thought can be changed.
 
 // As I say yes to life, life says yes to me.
