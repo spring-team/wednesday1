2c080419c0c7138707de06dce7df93752f085c60
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 17:51:44 +1100

    Everyone needs encouragement to write Java

--- src/main/java/com/ubs/Wednesday1Application.java
+++ src/main/java/com/ubs/Wednesday1Application.java
@@ -1,3 +1,5 @@
+// It’s only a thought, and a thought can be changed.
+
 // Deep at the center of my being is an infinite well of love.
 
 // I come from the loving space of my heart, and I know that love opens all doors.
