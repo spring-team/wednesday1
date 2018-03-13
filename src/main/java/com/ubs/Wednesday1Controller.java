0eeb6127fbbd009d26099764ac8301d8fd13b09e
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 19:42:29 +1100

    Everyone needs encouragement to write Java

--- src/main/java/com/ubs/Wednesday1Controller.java
+++ src/main/java/com/ubs/Wednesday1Controller.java
@@ -1,3 +1,5 @@
+// It’s only a thought, and a thought can be changed.
+
 // Deep at the center of my being is an infinite well of love.
 
 // You're good enough, you're smart enough, and doggone it, people like you.
