07753b24cfe5415e6efaa019457a8e5c0b15f71a
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 17:31:46 +1100

    Everyone needs encouragement to write Java

--- src/main/java/com/ubs/Wednesday1Application.java
+++ src/main/java/com/ubs/Wednesday1Application.java
@@ -1,5 +1,7 @@
 // I come from the loving space of my heart, and I know that love opens all doors.
 
+// I come from the loving space of my heart, and I know that love opens all doors.
+
 // Deep at the center of my being is an infinite well of love.
 
 package com.ubs;
